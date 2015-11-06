180374713ea7de42167312d109d82d753e80af56
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-11-06 15:22:48 +0000

    adds a little bit of test code to explore one way to approach this

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 var tests = require('./compat-table/data-es6').tests;
 
-console.log(tests[0]);
+for (var test of tests) {
+    if (test.name == 'class') {
+        var fullySupportedInVersion = 0;
+        
+        for (var subtest of test.subtests) {
+            Object.keys(subtest.res).some(function(k) {
+                if (~k.indexOf('chrome')) {
+                    var version = parseInt(k.substring(k.length-2, k.length), 10);
+                    
+                    if (version > fullySupportedInVersion) {
+                        fullySupportedInVersion = version;
+                    }
+                }
+            });
+        }
+        
+        console.log(test.name, 'is fully supported in chrome version ', fullySupportedInVersion);
+    }
+}
