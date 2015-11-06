cc7fe8a1cbc3e1ea3ca3b9076a8bda30bfcee200
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-11-06 15:25:00 +0000

    loops all tests instead of hard coded class

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var tests = require('./compat-table/data-es6').tests;
 
 for (var test of tests) {
-    if (test.name == 'class') {
-        var fullySupportedInVersion = 0;
-        
-        for (var subtest of test.subtests) {
-            Object.keys(subtest.res).some(function(k) {
-                if (~k.indexOf('chrome')) {
-                    var version = parseInt(k.substring(k.length-2, k.length), 10);
-                    
-                    if (version > fullySupportedInVersion) {
-                        fullySupportedInVersion = version;
-                    }
-                }
-            });
-        }
-        
-        console.log(test.name, 'is fully supported in chrome version ', fullySupportedInVersion);
+    if (!test['subtests']) {
+        continue;
     }
+    
+    var fullySupportedInVersion = 0;
+    
+    for (var subtest of test.subtests) {
+        Object.keys(subtest.res).some(function(k) {
+            if (~k.indexOf('chrome')) {
+                var version = parseInt(k.substring(k.length-2, k.length), 10);
+                
+                if (version > fullySupportedInVersion) {
+                    fullySupportedInVersion = version;
+                }
+            }
+        });
+    }
+    
+    console.log(test.name, 'is fully supported in chrome version ', fullySupportedInVersion);
 }
